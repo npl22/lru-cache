90b75c6df028f5526edfcd2427ccaf17c0225125
npl22 <nli22@jhu.edu>
2017-06-14 10:52:09 -0700

    Initial commit

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,105 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.length - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { [] }
+  end
+
+  def insert(num)
+    self[num] << num unless include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { [] }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if @count == @store.length
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
+  end
+
+  def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  # Key: use flatten to get all the numbers
+  def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    old_store.flatten.each { |num| insert(num) }
+  end
+end
