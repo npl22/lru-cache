737a5ac120426e025e32878bb709fc14caacdc3b
npl22 <nli22@jhu.edu>
2017-06-16 13:47:35 -0700

    Complete implementation of hash map

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source "https://rubygems.org"
 
 gem 'rspec', "~> 3.4.0"
-gem 'byebug'
-gem 'pry'
+gem 'pry-byebug'
--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,8 +1,10 @@
 require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
+require 'pry-byebug'
 
 class HashMap
-  attr_reader :count
+  include Enumerable
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -10,41 +12,67 @@ class HashMap
   end
 
   def include?(key)
+    self[key] ? true : false
   end
 
   def set(key, val)
+    resize! if @count >= num_buckets
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1 if bucket(key).remove(key)
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each { |node| yield([node.key, node.val]) }
+    end
+    nil
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
 
-  private
+  # private
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+
+    old_store.each do |bucket|
+      bucket.each { |node| set(node.key, node.val) }
+    end
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
+end
+
+# Testing
+if __FILE__ == $PROGRAM_NAME
+  h = HashMap.new
+  9.times { |i| h.set(i, i) }
 end
